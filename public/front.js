b54d49c0d5076cde11831473a05070aa279ae17e
hasham14fw <hasham14fw@gmail.com>
2025-08-15 18:35:07 +0500

    Purchased url

--- public/front.js
+++ public/front.js
@@ -1,4 +1,4 @@
-const backendURL = 'http://localhost:3000';
+const backendURL = 'aips-hsp.com';
 
 //============================= Loader Functions =============================
 function showLoading() {
